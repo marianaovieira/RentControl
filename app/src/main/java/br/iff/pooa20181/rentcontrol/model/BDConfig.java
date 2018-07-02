3efaf900b35b54e589aaedd12f1055a40f5e3e81
Mariana Vieira <marianaovieira1@gmail.com>
2018-07-02 12:01:52 -0300

    Configurando realm
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

---
+++ app/src/main/java/br/iff/pooa20181/rentcontrol/model/BDConfig.java
@@ -0,0 +1,23 @@
+package br.iff.pooa20181.rentcontrol.model;
+
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class BDConfig extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(getApplicationContext());
+
+        RealmConfiguration.Builder builder = new RealmConfiguration.Builder();
+        builder.name("eleicao.realm");
+        builder.schemaVersion(0);
+        builder.deleteRealmIfMigrationNeeded();
+        RealmConfiguration realmConfiguration = builder.build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+}
